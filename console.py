05b8295cd94691c32025e4fb354756d38740daf3
Kat-Devs <katlegophele95@gmail.com>
2023-03-16 19:23:45 +0200

    console create method added. Testing needed

--- console.py
+++ console.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 '''
-HBNBCommand intepreter
+Entry point for HBNBCommand intepreter
 '''
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
+class_dict = {
+    "BaseModel": BaseModel
+}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,11 +24,27 @@ class HBNBCommand(cmd.Cmd):
     
     def do_EOF(self, args):
         '''EOF command to exit the program'''
+        print()
         return True
     
     def emptyline(self):
         '''Do nothing on empty line'''
         pass
+
+    def do_create(self, arg):
+        '''Creates a new instance of BaseModel'''
+        if not arg:
+            print('** class name missing **')
+            return
+        args = arg.split()
+        class_name = args[0]
+        if class_name not in class_dict:
+            print('** class does not exist **')
+            return
+        instance = class_dict[class_name]()
+        instance.save()
+        print(instance.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
