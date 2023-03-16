69108fa9187aec2893c1ae4b56c24a5389729132
Kat-Devs <katlegophele95@gmail.com>
2023-03-16 13:39:53 +0000

    base_model pycodestyle update

--- models/base_model.py
+++ models/base_model.py
@@ -3,22 +3,23 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     '''
     Base class for other models to inherit from.
-    Provides common attributes and methods    
+    Provides common attributes and methods
     '''
 
     def __init__(self):
         ''' Inits a new instance of BaseModel'''
-        self.id = str(uuid.uuid4()) 
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         ''' Returns string rep of instance'''
         return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
-    
+
     def save(self):
         ''' Update updated_at with current datetime'''
         self.updated_at = datetime.now()
@@ -32,4 +33,4 @@ class BaseModel:
         new_dict['__class__'] = type(self).__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.created_at.isoformat()
-        return new_dict 
+        return new_dict
