c387ce9c4e66ec7015951a63a7e089acf4564026
Kat-Devs <katlegophele95@gmail.com>
2023-03-16 19:01:21 +0200

    console.py added

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+HBNBCommand intepreter
+'''
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''
+    Command Intepreter Class
+    '''
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        '''Quit command to exit program'''
+        return True
+    
+    def do_EOF(self, args):
+        '''EOF command to exit the program'''
+        return True
+    
+    def emptyline(self):
+        '''Do nothing on empty line'''
+        pass
