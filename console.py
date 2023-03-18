f727b9d0422eb033d379a2d9a4f1bdb9b20778ea
Kat-Devs <katlegophele95@gmail.com>
2023-03-18 16:02:36 +0200

    console - no added functions

--- console.py
+++ console.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Entry point of the command interpreter"""
-
+"""
+This module defines the entry point of the command interpreter.
+"""
 import cmd
-from models.base_model import BaseModel
-
-classes = {"BaseModel": BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
+    """
+    This class defines the entry point of the command interpreter.
+    """
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -20,21 +21,9 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """Called when an empty line is entered"""
+        """Do nothing when an empty line is entered"""
         pass
 
-    def do_create(self, arg):
-        """Create a new instance of BaseModel"""
-        if not arg:
-            print("** class name missing **")
-        else:
-            args = arg.split()
-            if args[0] != "BaseModel":
-                print("** class doesn't exist **")
-            else:
-                new_instance = BaseModel()
-                new_instance.save()
-                print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
