273dffb38eba85cb1d0b1629af2e26b036d8e8b7
Katlego <113664624+Kat-Devs@users.noreply.github.com>
2023-03-19 17:43:58 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance of the BaseModel class"""
-        self.id = (uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
 
